dfce0fca183fed0da4b95bb289176d669ab9a7e5
dylan-asmar <asmar@stanford.edu>
2022-09-14 01:53:49 -0700

    sim_updates

--- run_sims.jl
+++ run_sims.jl
@@ -1,45 +1,49 @@
 using MPOPIS
 
-# simulate_envpool_env(
-#     "Swimmer-v4";
-#     frame_skip = 4,
-#     num_trials = 5,
-#     policy_type = :mppi,
-#     num_steps = 50,
-#     num_samples = 1500,
-#     λ = 0.1,
-# )
-
-# simulate_envpool_env(
-#     "Swimmer-v4";
-#     frame_skip = 4,
-#     num_trials = 5,
-#     policy_type = :cemppi,
-#     num_steps = 50,
-#     num_samples = 300,
-#     λ = 0.1,
-#     ais_its = 5,
-#     ce_Σ_est = :ss,
-# )
+simulate_envpool_env(
+    "Swimmer-v4";
+    frame_skip = 4,
+    num_trials = 20,
+    policy_type = :mppi,
+    num_steps = 100,
+    num_samples = 3000,
+    λ = 0.1,
+    seed = 1,
+)
 
 simulate_envpool_env(
-    "Ant-v4";
-    frame_skip = 5,
-    num_trials = 5,
-    policy_type = :mppi,
-    num_steps = 20,
-    num_samples = 3000,
-    λ = 1.0,
+    "Swimmer-v4";
+    frame_skip = 4,
+    num_trials = 20,
+    policy_type = :cemppi,
+    num_steps = 100,
+    num_samples = 600,
+    ais_its = 5,
+    λ = 0.1,
+    ce_Σ_est = :ss,
+    seed = 1,
 )
 
 # simulate_envpool_env(
 #     "Ant-v4";
 #     frame_skip = 5,
 #     num_trials = 5,
-#     policy_type = :cemppi,
-#     num_steps = 20,
-#     num_samples = 300,
+#     policy_type = :mppi,
+#     num_steps = 100,
+#     num_samples = 3000,
 #     λ = 1.0,
-#     ais_its = 5,
+#     seed = 1,
+# )
+
+# simulate_envpool_env(
+#     "Ant-v4";
+#     frame_skip = 5,
+#     num_trials = 5,
+#     policy_type = :cemppi,
+#     num_steps = 100,
+#     num_samples = 375,
+#     λ = 1.0,
+#     ais_its = 8,
 #     ce_Σ_est = :ss,
+#     seed = 1,
 # )
