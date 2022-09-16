cf71808f8ea22eb23f15f1d0e92f4156b8a4c5a0
Dylan Asmar <asmar@stanford.edu>
2022-09-15 17:45:44 -0700

    repo cleanup after submission

--- run_sims.jl
+++ run_sims.jl
@@ -26,26 +26,42 @@ simulate_envpool_env(
     cov_mat = [0.25 0.0; 0.0 0.25],
 )
 
-# simulate_envpool_env(
-#     "Ant-v4";
-#     frame_skip = 5,
-#     num_trials = 20,
-#     policy_type = :mppi,
-#     num_steps = 100,
-#     num_samples = 900,
-#     λ = 1.0,
-#     seed = 1,
-# )
+simulate_envpool_env(
+    "Ant-v4";
+    frame_skip = 5,
+    num_trials = 10,
+    policy_type = :mppi,
+    num_steps = 250,
+    num_samples = 900,
+    λ = 1.0,
+    seed = 1,
+    cov_mat = Matrix(I(8)*0.25),
+)
 
-# simulate_envpool_env(
-#     "Ant-v4";
-#     frame_skip = 5,
-#     num_trials = 20,
-#     policy_type = :cemppi,
-#     num_steps = 100,
-#     num_samples = 300,
-#     ais_its = 3,
-#     λ = 1.0,
-#     ce_Σ_est = :ss,
-#     seed = 1,
-# )
+simulate_envpool_env(
+    "Ant-v4";
+    frame_skip = 5,
+    num_trials = 10,
+    policy_type = :cemppi,
+    num_steps = 250,
+    num_samples = 300,
+    ais_its = 3,
+    λ = 1.0,
+    ce_Σ_est = :ss,
+    seed = 1,
+    cov_mat = Matrix(I(8)*0.25),
+)
+
+simulate_envpool_env(
+    "HalfCheetah-v4";
+    frame_skip = 5,
+    num_trials = 10,
+    policy_type = :cemppi,
+    num_steps = 250,
+    num_samples = 100,
+    ais_its = 5,
+    λ = 1.0,
+    ce_Σ_est = :ss,
+    seed = 1,
+    cov_mat = Matrix(I(6)*0.25),
+)
